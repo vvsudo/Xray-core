111a345176f3947e74839d3c38d279d255dda174
ADMIN <jonsee8522@qq.com>
2022-12-29 11:08:57 +0800

    ax v1.6.1

---
+++ core/context.go
@@ -0,0 +1,50 @@
+package core
+
+import (
+	"context"
+)
+
+// XrayKey is the key type of Instance in Context, exported for test.
+type XrayKey int
+
+const xrayKey XrayKey = 1
+
+// FromContext returns an Instance from the given context, or nil if the context doesn't contain one.
+func FromContext(ctx context.Context) *Instance {
+	if s, ok := ctx.Value(xrayKey).(*Instance); ok {
+		return s
+	}
+	return nil
+}
+
+// MustFromContext returns an Instance from the given context, or panics if not present.
+func MustFromContext(ctx context.Context) *Instance {
+	x := FromContext(ctx)
+	if x == nil {
+		panic("X is not in context.")
+	}
+	return x
+}
+
+func WithContext(ctx context.Context, v *Instance) context.Context {
+	if FromContext(ctx) != v {
+		ctx = context.WithValue(ctx, xrayKey, v)
+	}
+	return ctx
+}
+
+func toContext(ctx context.Context, v *Instance) context.Context {
+	if FromContext(ctx) != v {
+		ctx = context.WithValue(ctx, xrayKey, v)
+	}
+	return ctx
+}
+
+/*
+ToBackgroundDetachedContext create a detached context from another context
+Internal API
+*/
+func ToBackgroundDetachedContext(ctx context.Context) context.Context {
+	instance := MustFromContext(ctx)
+	return toContext(context.Background(), instance)
+}
